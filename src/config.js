54c5a6500771c39297e74ff4517208b3bf05f1b9
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-07-27 01:48:08 -0400

    secret remove

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const HOST_WEB = process.env.HOST_WEB ||'http://localhost:8080';
 const DB_HOST = process.env.DB_HOST ||'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
-const DB_NAME = process.env.DB_NAME || 'sw1pizarraC4';
+const DB_NAME = process.env.DB_NAME || 'you_pass';
 const DB_PORT = process.env.DB_PORT || 3306;
 
 const API_KEY_OPENIA = process.env.API_KEY_OPENIA || "your-secret-api-key";
