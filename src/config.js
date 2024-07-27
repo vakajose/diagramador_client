a0006d311b22e5a56afdda6f6caf826995140220
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2024-07-27 01:49:09 -0400

    remove secrets

--- src/config.js
+++ src/config.js
@@ -4,11 +4,11 @@ const HOST_WEB = process.env.HOST_WEB ||'http://localhost:8080';
 
 const DB_HOST = process.env.DB_HOST ||'localhost';
 const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'root';
-const DB_NAME = process.env.DB_NAME || 'you_pass';
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 const DB_PORT = process.env.DB_PORT || 3306;
 
-const API_KEY_OPENIA = process.env.API_KEY_OPENIA || "your-secret-api-key";
+const API_KEY_OPENIA = process.env.API_KEY_OPENIA;
 
 module.exports = {
   PORT,
